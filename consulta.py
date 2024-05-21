61773db669fe01dfbb8c423db435803a37b4f564
HydroAndrew22 <andrew022@misena.edu.co>
2024-05-20 20:49:50 -0500

    Versión 1 del proyecto

---
+++ consulta.py
@@ -0,0 +1,21 @@
+import psycopg2
+from conexion import *
+
+DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD = consultar_db ()
+# Conexión
+try:
+    conn = psycopg2.connect(
+        dbname=DB_NAME, user=DB_USER,
+        password=DB_PASSWORD, host=DB_HOST, port=DB_PORT
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tab_empleados")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
+except psycopg2.Error as e:
+    print("Error al conectar a la base de datos:", e)
+
+
---
+++ sql.sql
@@ -0,0 +1,92 @@
+-- CREACION DE LA BASE DE DATOS
+CREATE DATABASE bd_clinica with owner=postgres encoding='UTF8';
+
+-- ELIMINAR LAS TABLAS SI EXISTEN
+DROP TABLE IF EXISTS tab_agendar_cita, tab_empleados, tab_especialidades, tab_tipo_cita, tab_usuarios;
+
+
+-- CREACION DE LAS TABLAS
+CREATE TABLE tab_usuarios (
+    id_usuario VARCHAR(50) NOT NULL, 
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
+    departamento VARCHAR(50) NOT NULL,
+    ciudad VARCHAR(50) NOT NULL,
+    fecha_retiro TIMESTAMP NULL,
+    esactivo BOOLEAN DEFAULT true NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id_usuario)
+) ;
+
+CREATE TABLE tab_especialidades (
+    id_especialidad SERIAL, 
+    tipo VARCHAR(50) NOT NULL,
+    esactivo BOOLEAN DEFAULT true NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --  ON UPDATE CURRENT_TIMESTAMP,  
+    PRIMARY KEY (id_especialidad)
+) ;
+
+
+CREATE TABLE tab_empleados (
+    id_profesional SERIAL, 
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    id_especialidad int NOT NULL,
+    sede VARCHAR(50) NOT NULL,
+    esactivo BOOLEAN DEFAULT true NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+    FOREIGN KEY (id_especialidad) REFERENCES tab_especialidades(id_especialidad),
+    PRIMARY KEY (id_profesional)
+) ;
+
+
+CREATE TABLE tab_tipo_cita (
+    id_tipo_cita SERIAL, 
+    tipo VARCHAR(50) NOT NULL,
+    esactivo BOOLEAN DEFAULT true NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+    PRIMARY KEY (id_tipo_cita)
+) ;
+
+
+
+CREATE TYPE estado_cita_enum AS ENUM ('PENDIENTE', 'REALIZADA', 'CANCELADA', 'CONFIRMADA');
+CREATE TABLE tab_agendar_cita (
+    id_ac SERIAL PRIMARY KEY,
+    id_usuario VARCHAR(50) NOT NULL,
+    id_tipo_cita INT NOT NULL,
+    id_profesional INT NOT NULL,
+    fecha_cita TIMESTAMP NOT NULL,
+    estado_cita estado_cita_enum DEFAULT 'PENDIENTE',
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_usuario) REFERENCES tab_usuarios(id_usuario),
+    FOREIGN KEY (id_tipo_cita) REFERENCES tab_tipo_cita(id_tipo_cita),
+    FOREIGN KEY (id_profesional) REFERENCES tab_empleados(id_profesional)
+);
+
+
+    
+
+-- INSERT
+INSERT INTO tab_especialidades (tipo) VALUES ('Cardiólogo'), ('Dermatólogo'), ('Ginecobstetra'), ('Ginecólogo'), ('Internista'), ('Médico General'), ('Nutricionista'), ('Oftalmólogo'), ('Ortopedista'), ('Otorrinolaringólogo'), ('Pediatra'), ('Urólogo');
+
+INSERT INTO tab_tipo_cita (tipo) VALUES  ('Cardiología'), ('Dermatología'), ('Ginecobstetricia'), ('Ginecología'), ('Internista'), ('Medicina General'), ('Nutricionista'), ('Oftalmología'), ('Ortopédia'), ('Otorrinolaringología'), ('Pediatría'), ('Urología');
+
+INSERT INTO tab_empleados (nombre, apellido, email, id_especialidad, sede) VALUES  ('Juan Camilo','Reyes','quam.curabitur@aol.net', 1,'Centro'),  ('Carlos','Santodomingo','non.justo.proin@hotmail.org', 2,'Manila'),  ('erick','Leon','auctor.vitae@aol.edu',10,'Argentina'),  ('Oscar','Betancourt','risus@hotmail.ca',5,'Poblado'),  ('Franco','Escamilla','dictum.cursus@icloud.couk',3,'Rio'), ('Cameron','Díaz','amet@protonmail.com',9,'Bello'), ('Rosalia','Mejía','nec.tellus@icloud.org',8,'La Frontera'), ('Eliana','Barrios','ut.nisi.a@icloud.ca',7,'Manrique'), ('Lorena','Cano','egestas.fusce@icloud.net',6,'Itagui'), ('María','giraldo','lorem.vitae.odio@yahoo.org',4,'Caldas'), ('Juana','Umaña','cursus.nunc@google.edu',11,'Sabaneta'), ('Leidy','Muñoz','convallis.est@aol.edu',12,'Enciso');
+
+
+
+-- VISTAS
+
+create view vw_agendar_cita as SELECT ac.id_ac, ac.id_usuario, ac.id_tipo_cita, ac.fecha_cita, ac. estado_cita, em.id_profesional, 
+CONCAT(em.nombre,' ', em.apellido) as nombre_profesional, em.id_especialidad, em.sede, tp.tipo 
+FROM tab_agendar_cita ac 
+LEFT join tab_empleados em on em.id_profesional = ac.id_profesional 
+LEFT join tab_tipo_cita tp on tp.id_tipo_cita = ac.id_tipo_cita
+WHERE ac.estado_cita='PENDIENTE' ;
+
+
